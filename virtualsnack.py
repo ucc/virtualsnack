4dd48502f2f016b13284dd20f7916e2aa0896876
Mark Tearle <mark@tearle.com>
2014-10-12 23:24:39 +0800

    Add initial widgets for buttons

--- virtualsnack.py
+++ virtualsnack.py
@@ -2,6 +2,9 @@
 import npyscreen
 from datetime import datetime
 
+class ContainedMultiSelect(npyscreen.BoxTitle):
+    _contained_widget = npyscreen.TitleMultiSelect
+
 class VirtualSnack(npyscreen.Form):
 
     def while_waiting(self):
@@ -22,6 +25,21 @@ class VirtualSnack(npyscreen.Form):
         self.date_widget.value = "Hello"
 	self.add_handlers({"^T": self.exit_application})
 
+	self.dip = self.add(npyscreen.MultiSelect, name = "DIP Switch", max_width = 45, relx = 2, max_height=10, value = [], values = ["DIP1", "DIP2", "DIP3","DIP4","DIP5","DIP6","DIP7","DIP8"], scroll_exit=True)
+	self.dip = self.add(npyscreen.MultiSelect, name = "Door", max_width=15, relx = 2, max_height=4, value = [], values = ["DOOR"], scroll_exit=True)
+
+	self.nickel= self.add(npyscreen.MiniButton,name="0.05")
+	self.dime= self.add(npyscreen.MiniButton,name="0.10")
+	self.quarter= self.add(npyscreen.MiniButton,name="0.25")
+	self.dollar= self.add(npyscreen.MiniButton,name="1.00")
+
+	self.mode= self.add(npyscreen.MiniButton,name="MODE")
+	self.reset= self.add(npyscreen.MiniButton,name="RESET")
+
+	self.kpbuttons = []
+	for keypad in range(0,10):
+		self.kpbuttons.append(self.add(npyscreen.MiniButton,name="%d"%keypad))
+		
 
     def exit_application(self,name):
         self.parentApp.setNextForm(None)
